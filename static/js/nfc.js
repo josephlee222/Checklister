66973cea03fcb09de57da080a8e6e9caeab642ab
Joseph Lee <facebooklee52@gmail.com>
2023-10-13 22:37:15 +0800

    Fixed NFC error on init

--- static/js/nfc.js
+++ static/js/nfc.js
@@ -1,9 +1,8 @@
-async function writeNfc(url) {
-    if (!('NDEFWriter' in window)) {
-        changeStatus("portable_wifi_off", "NFC not supported on this device or browser")
-        return
-    }
+// if (!('NDEFWriter' in window)) {
+//     changeStatus("portable_wifi_off", "NFC not supported on this device or browser")
+// }
 
+async function writeNfc(url) {
     try {
         const ndef = new NDEFReader();
         changeStatus("wifi_tethering", "Place NFC tag on the back of the phone")
@@ -12,13 +11,15 @@ async function writeNfc(url) {
         });
         changeStatus("done", "NFC tag has been written")
     } catch (error) {
-        changeStatus("warning", "Error while initializing NFC")
+        changeStatus("warning", "Error while initializing NFC - " + error.message )
     }
 }
 
-function changeStatus(icon, text) {
+function changeStatus(icon, text, button=true) {
     var i = document.getElementById("nfc-status-icon")
     var s = document.getElementById("nfc-status-text")
+    var b = document.getElementById("nfc-status-button")
     i.innerHTML = icon
     s.innerHTML = text
+    b.disabled = button
 }
